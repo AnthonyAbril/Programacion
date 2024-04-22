7577a6553998905dac0b0a461ced218a104678e7
AnthonyAbril <antabrrue@alu.edu.gva.es>
2024-04-22 16:58:18 +0200

    añadido el menu y el metodo de altalibro

--- Tema12/Gestion_de_Biblioteca/src/gestion_de_biblioteca/Principal.java
+++ Tema12/Gestion_de_Biblioteca/src/gestion_de_biblioteca/Principal.java
@@ -2,8 +2,10 @@
 package gestion_de_biblioteca;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Principal {
+    static Scanner sc = new Scanner(System.in);
 
     //- Colección empleados/as de la biblioteca: esta colección estará precargada por programa e
     //incluirá las siguientes personas: Alberto, Encarna, Estela, Manolo, Agustín. En la opción
@@ -23,6 +25,65 @@ public class Principal {
         if(true){
             Empleados.add(new Empleado());
         }
+        
+        Menu();
     }
     
+    public static void Menu(){
+        int opcion=0;
+        
+        do{
+            System.out.print("------ Menu --------------------------"
+                    + "\n\t1- Dar de alta un libro en el sistema"
+                    + "\n\t2- Búsqueda de libros dentro del sistema"
+                    + "\n\t3- Dar de baja un libro en el sistema"
+                    + "\n\t4- Alquiler de un libro por un usuario"
+                    + "\n\t5- Devolución de un libro por un usuario"
+                    + "\n\t6- Gestión de empleados/as de la biblioteca"
+                    + "\n\t7- Gestión de usuarios/as de la biblioteca"
+                    + "\n\t8- Salir del sistema."
+                    + ">Elige una opcion: ");
+            
+            switch (opcion) {
+                case 1:
+                    AltaLibro();
+                    break;
+                case 2:
+                    BuscarLibro();
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                default:
+                    break;
+            }
+        }while(opcion!=8);
+    }
+    
+    public static void AltaLibro(){
+        //El sistema pedirá: título, autor, editorial, ubicación, ISBN, precio.
+        //El sistema añadirá este libro en el conjunto de libros de la biblioteca.
+        
+        System.out.print(">Introduce el titulo del libro: ");
+        String titulo = sc.nextLine();
+        System.out.print(">Introduce el autor del libro: ");
+        String autor = sc.nextLine();
+        System.out.print(">Introduce la editorial del libro: ");
+        String editorial = sc.nextLine();
+        System.out.print(">Introduce el ISBN del libro: ");
+        String ISBN = sc.nextLine();
+        System.out.print(">Introduce la ubicación del libro (numero del pasillo): ");
+        int pasillo = sc.nextInt();
+        System.out.print(">Introduce el precio del libro: ");
+        Double precio = sc.nextDouble();
+        
+        libros.add(new Libro(titulo,autor,editorial,ISBN,pasillo,precio));
+    }
+    
+    public static void BuscarLibro(){
+        
+    }
 }
